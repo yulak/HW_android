2f458d43a7119684948fe62989adc0fb3975af9f
yulak <dyulakov@mail.ru>
2020-08-16 23:18:26 +0400

    Initial commit

---
+++ app/src/main/java/myApp/test1/MainActivity.java
@@ -0,0 +1,28 @@
+package myApp.test1;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+      final TextView cityTextView = findViewById(R.id.nameCity);
+      Button buttonCity = findViewById(R.id.buttonCity);
+
+      buttonCity.setOnClickListener(new View.OnClickListener() {
+          @Override
+          public void onClick(View view) {
+              cityTextView.setText(R.string.textCity);
+          }
+      });
+
+    }
+}
